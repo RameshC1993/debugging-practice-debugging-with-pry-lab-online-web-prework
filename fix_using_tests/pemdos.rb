b32112082b96ee8505ce71536a75233b70cd42b2
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 21:22:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
 def snake_it_up(string)
+  newstr = string
   if string[0] == "s"
     10.times do 
-      "s" + string
+      newstr = "s" + newstr
     end
   else
   string
   end
+  return newstr
 end
